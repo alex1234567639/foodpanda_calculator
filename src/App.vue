48bba6d20f8fc4138712803413853e2dc2542fb5
alex1234567639 <alex8603062000@gmail.com>
2021-02-02 22:43:37 +0800

    add banner img

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="今晚，我想來點..."/>
+    <img class="img" alt="Vue logo" src="./assets/uberEat.png">
   </div>
 </template>
 
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.img {
+  width: 900px;
 }
 </style>
